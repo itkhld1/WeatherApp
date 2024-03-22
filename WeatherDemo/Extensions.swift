dc9fca10ac79053425e9c7913e197a5fd71421a6
khlaid samim <95288187+itkhld@users.noreply.github.com>
2024-03-22 20:36:11 +0300

    WeatherApp

---
+++ WeatherDemo/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  WeatherDemo
+//
+//  Created by Itkhld on 22.03.2024.
+//
+
+import Foundation
+
+extension Double {
+    func roundDouble() -> String {
+        return String(format: "%.0f", self)
+    }
+}
--- WeatherDemo/WeatherDemo/Views/WeatherView.swift
+++ WeatherDemo/WeatherDemo/Views/WeatherView.swift
@@ -11,7 +11,25 @@ struct WeatherView: View {
     var weather: ResponseBody
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .leading){
+            VStack{
+                VStack(alignment: .leading, spacing: 5){
+                    Text(weather.name)
+                        .bold().font(.title)
+                    
+                    Text("Today, \(Date().formatted(.dateTime.month().day().hour().minute()))")
+                        .fontWeight(.light)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Spacer()
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .background(Color(hue: 0.587, saturation: 0.78, brightness: 0.377))
+        .preferredColorScheme(.dark)
     }
 }
 
