fd191feea24609adf8b26ac4d81c77f733090657
khlaid samim <95288187+itkhld@users.noreply.github.com>
2024-03-24 14:35:02 +0300

    WeatherApp

--- WeatherDemo/WeatherDemo/Views/WeatherView.swift
+++ WeatherDemo/WeatherDemo/Views/WeatherView.swift
@@ -32,17 +32,47 @@ struct WeatherView: View {
                             
                             Text(weather.weather[0].main)
                         }
+                        .frame(width: 150, alignment: .leading)
+                        
+                        Spacer()
                         
                         Text(weather.main.feelsLike.roundDouble() + "˚")
                             .font(.system(size: 100))
                             .fontWeight(.bold)
                             .padding()
                     }
+                    Spacer()
+                        .frame(height: 80)
+                    
+                    AsyncImage(url: URL(string: "https://pixabay.com/vectors/landscape-twilight-light-night-1844226/")){
+                        image in image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 350)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    Spacer()
+                    
                 }
                 .frame(maxWidth: .infinity)
             }
             .padding()
             .frame(maxWidth: .infinity, alignment: .leading)
+            
+            VStack {
+                Spacer()
+                
+                VStack(alignment: .leading, spacing: 20) {
+                    
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .padding(.bottom, 20)
+                .foregroundColor(Color(hue: 0.587, saturation: 0.78, brightness: 0.377))
+                .background(.white)
+                .cornerRadius(20, corners: [.topLeft, .topRight])
+            }
         }
         .edgesIgnoringSafeArea(.bottom)
         .background(Color(hue: 0.587, saturation: 0.78, brightness: 0.377))
